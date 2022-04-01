3a45f2729ddce80f9d689b7b58d2076e55a1342c
Romain Dreidemy <dreidemyromain@gmail.com>
2022-04-01 18:43:06 +0200

    migration style with tailwindcss

---
+++ config/tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      height: {
+        'banner': 'calc(100vh - 64px)'
+      },
+      width: {
+        'container': '500px'
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
